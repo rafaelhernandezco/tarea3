7a9b30091a4a8248c13e68712c6b69d92a2a97fd
rafaelhernandezco <rafael.hcarrasco30@gmail.com>
2015-10-02 10:29:35 -0500

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -1,1 +1,51 @@
 #Solution File
+class Node
+	attr_accessor :data, :ref
+
+	def initialize data,ref
+		@data=data
+		@ref=ref
+	end
+    
+end
+
+class List
+    
+	def initialize data
+		@header=Node.new(data,nil)
+	end
+	
+	def add(data)
+		direction=@header
+        
+		while direction.ref !=nil
+			direction=direction.ref
+		end
+		direction.ref=Node.new(data,nil)
+	end
+
+	def imprimir
+		direction=@header
+		list = []
+        
+		while direction.ref !=nil
+			list = list +[direction.data.to_s]
+			direction = direction.ref
+		end
+		list += [direction.data.to_s]
+		puts list.join(",")
+        
+	end
+end
+	data = gets.chomp
+	list = List.new(data)
+
+	while data != "-1"
+		data = gets.chomp
+        
+		if data != "-1"
+			list.add(data)
+	end
+end
+
+puts list.imprimir
